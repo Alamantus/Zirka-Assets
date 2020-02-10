4e3b440ea853c3489bbc53bff5ac8ce87b44f413
Robbie Antenesse <dev@alamantus.com>
2020-02-10 12:08:10 -0700

    Move Zirka functions into its own object

--- translate.js
+++ translate.js
@@ -1,204 +1,206 @@
-var characterMap = {
-  a: {
-    character: 'a',
-    pronunciation: 'u',
-  },
-  ai: {
-    character: 'ai',
-    pronunciation: 'ua',
-  },
-  b: {
-    character: 'b',
-    pronunciation: 'k',
-  },
-  ch: {
-    character: 'ch',
-    pronunciation: 'th',
-  },
-  ':': {
-    character: 'colon',
-    pronunciation: ':',
-  },
-  '"': {
-    character: 'colon',
-    pronunciation: ':',
-  },
-  ',': {
-    character: 'comma',
-    pronunciation: ',',
-  },
-  d: {
-    character: 'd',
-    pronunciation: 'b',
-  },
-  dr: {
-    character: 'dr',
-    pronunciation: 'bis',
-  },
-  e: {
-    character: 'e',
-    pronunciation: 'o',
-  },
-  '!': {
-    character: 'exclamation',
-    pronunciation: '!',
-  },
-  f: {
-    character: 'f',
-    pronunciation: 'sh',
-  },
-  fl: {
-    character: 'fl',
-    pronunciation: 'shr',
-  },
-  fr: {
-    character: 'fr',
-    pronunciation: 'z',
-  },
-  g: {
-    character: 'g',
-    pronunciation: 'p',
-  },
-  gr: {
-    character: 'gr',
-    pronunciation: 'pas',
-  },
-  h: {
-    character: 'h',
-    pronunciation: 'n',
-  },
-  i: {
-    character: 'i',
-    pronunciation: 'a',
-  },
-  j: {
-    character: 'jz',
-    pronunciation: 'w',
-  },
-  k: {
-    character: 'k',
-    pronunciation: 't',
-  },
-  kl: {
-    character: 'kl',
-    pronunciation: 'tr',
-  },
-  kr: {
-    character: 'kr',
-    pronunciation: 'ts',
-  },
-  l: {
-    character: 'l',
-    pronunciation: 'd',
-  },
-  m: {
-    character: 'm',
-    pronunciation: 'd',
-  },
-  n: {
-    character: 'n',
-    pronunciation: 'f',
-  },
-  o: {
-    character: 'o',
-    pronunciation: 'e',
-  },
-  p: {
-    character: 'p',
-    pronunciation: 'm',
-  },
-  '.': {
-    character: 'period',
-    pronunciation: '.',
-  },
-  pr: {
-    character: 'pr',
-    pronunciation: 'mus',
-  },
-  '?': {
-    character: 'question',
-    pronunciation: '?',
-  },
-  r: {
-    character: 'r',
-    pronunciation: 's',
-  },
-  s: {
-    character: 's',
-    pronunciation: 'l',
-  },
-  sh: {
-    character: 'sh',
-    pronunciation: 'kl',
-  },
-  sk: {
-    character: 'sk',
-    pronunciation: 'lat',
-  },
-  sl: {
-    character: 'sl',
-    pronunciation: 'lr',
-  },
-  sm: {
-    character: 'sm',
-    pronunciation: 'led',
-  },
-  sn: {
-    character: 'sn',
-    pronunciation: 'luf',
-  },
-  sp: {
-    character: 'sp',
-    pronunciation: 'lum',
-  },
-  st: {
-    character: 'st',
-    pronunciation: 'lag',
-  },
-  sw: {
-    character: 'sw',
-    pronunciation: 'luv',
-  },
-  t: {
-    character: 't',
-    pronunciation: 'g',
-  },
-  th: {
-    character: 'th',
-    pronunciation: 'j',
-  },
-  tr: {
-    character: 'tr',
-    pronunciation: 'gis',
-  },
-  tw: {
-    character: 'tw',
-    pronunciation: 'giv',
-  },
-  u: {
-    character: 'u',
-    pronunciation: 'i',
-  },
-  v: {
-    character: 'vw',
-    pronunciation: 'v',
-  },
-  w: {
-    character: 'vw',
-    pronunciation: 'v',
-  },
-  y: {
-    character: 'y',
-    pronunciation: 'ch',
-  },
-  z: {
-    character: 'jz',
-    pronunciation: 'w',
-  },
+window.Zirka = {
+  characterMap: {
+    a: {
+      character: 'a',
+      pronunciation: 'u',
+    },
+    ai: {
+      character: 'ai',
+      pronunciation: 'ua',
+    },
+    b: {
+      character: 'b',
+      pronunciation: 'k',
+    },
+    ch: {
+      character: 'ch',
+      pronunciation: 'th',
+    },
+    ':': {
+      character: 'colon',
+      pronunciation: ':',
+    },
+    '"': {
+      character: 'colon',
+      pronunciation: ':',
+    },
+    ',': {
+      character: 'comma',
+      pronunciation: ',',
+    },
+    d: {
+      character: 'd',
+      pronunciation: 'b',
+    },
+    dr: {
+      character: 'dr',
+      pronunciation: 'bis',
+    },
+    e: {
+      character: 'e',
+      pronunciation: 'o',
+    },
+    '!': {
+      character: 'exclamation',
+      pronunciation: '!',
+    },
+    f: {
+      character: 'f',
+      pronunciation: 'sh',
+    },
+    fl: {
+      character: 'fl',
+      pronunciation: 'shr',
+    },
+    fr: {
+      character: 'fr',
+      pronunciation: 'z',
+    },
+    g: {
+      character: 'g',
+      pronunciation: 'p',
+    },
+    gr: {
+      character: 'gr',
+      pronunciation: 'pas',
+    },
+    h: {
+      character: 'h',
+      pronunciation: 'n',
+    },
+    i: {
+      character: 'i',
+      pronunciation: 'a',
+    },
+    j: {
+      character: 'jz',
+      pronunciation: 'w',
+    },
+    k: {
+      character: 'k',
+      pronunciation: 't',
+    },
+    kl: {
+      character: 'kl',
+      pronunciation: 'tr',
+    },
+    kr: {
+      character: 'kr',
+      pronunciation: 'ts',
+    },
+    l: {
+      character: 'l',
+      pronunciation: 'd',
+    },
+    m: {
+      character: 'm',
+      pronunciation: 'd',
+    },
+    n: {
+      character: 'n',
+      pronunciation: 'f',
+    },
+    o: {
+      character: 'o',
+      pronunciation: 'e',
+    },
+    p: {
+      character: 'p',
+      pronunciation: 'm',
+    },
+    '.': {
+      character: 'period',
+      pronunciation: '.',
+    },
+    pr: {
+      character: 'pr',
+      pronunciation: 'mus',
+    },
+    '?': {
+      character: 'question',
+      pronunciation: '?',
+    },
+    r: {
+      character: 'r',
+      pronunciation: 's',
+    },
+    s: {
+      character: 's',
+      pronunciation: 'l',
+    },
+    sh: {
+      character: 'sh',
+      pronunciation: 'kl',
+    },
+    sk: {
+      character: 'sk',
+      pronunciation: 'lat',
+    },
+    sl: {
+      character: 'sl',
+      pronunciation: 'lr',
+    },
+    sm: {
+      character: 'sm',
+      pronunciation: 'led',
+    },
+    sn: {
+      character: 'sn',
+      pronunciation: 'luf',
+    },
+    sp: {
+      character: 'sp',
+      pronunciation: 'lum',
+    },
+    st: {
+      character: 'st',
+      pronunciation: 'lag',
+    },
+    sw: {
+      character: 'sw',
+      pronunciation: 'luv',
+    },
+    t: {
+      character: 't',
+      pronunciation: 'g',
+    },
+    th: {
+      character: 'th',
+      pronunciation: 'j',
+    },
+    tr: {
+      character: 'tr',
+      pronunciation: 'gis',
+    },
+    tw: {
+      character: 'tw',
+      pronunciation: 'giv',
+    },
+    u: {
+      character: 'u',
+      pronunciation: 'i',
+    },
+    v: {
+      character: 'vw',
+      pronunciation: 'v',
+    },
+    w: {
+      character: 'vw',
+      pronunciation: 'v',
+    },
+    y: {
+      character: 'y',
+      pronunciation: 'ch',
+    },
+    z: {
+      character: 'jz',
+      pronunciation: 'w',
+    },
+  }
 }
 
 // getMappedLetters returns an array of strings that can be mapped to character file names.
-function getMappedLetters(string, leaveIfNotExists) {
+Zirka.getMappedLetters = function (string, leaveIfNotExists) {
   var result = [];
   var characters = string.toLowerCase().split('');
   for (var i = 0; i < characters.length; i++) {
@@ -206,17 +208,17 @@ function getMappedLetters(string, leaveIfNotExists) {
     if (typeof characters[i + 1] !== 'undefined') {
       // If the next character exists, check if the digraph is in the map.
       var digraph = character + characters[i + 1];
-      if (typeof characterMap[digraph] !== 'undefined') {
+      if (typeof Zirka.characterMap[digraph] !== 'undefined') {
         // If the digraph exists, push it to the results and skip the next index.
-        result.push(characterMap[digraph]);
+        result.push(Zirka.characterMap[digraph]);
         i++;
         continue;
       }
     }
     
-    if (typeof characterMap[character] !== 'undefined') {
+    if (typeof Zirka.characterMap[character] !== 'undefined') {
       // If single character is found, push it to results.
-      result.push(characterMap[character]);
+      result.push(Zirka.characterMap[character]);
       continue;
     }
     
@@ -225,7 +227,7 @@ function getMappedLetters(string, leaveIfNotExists) {
   return result;
 }
 
-function insertCharacters(string, container, options) {
+Zirka.insertCharacters = function (string, container, options) {
   options = typeof options !== 'undefined' ? options : {};
   options.display = typeof options.display !== 'undefined' ? options.display : 'both';
   options.type = typeof options.type !== 'undefined' ? options.type : 'block';
@@ -237,7 +239,7 @@ function insertCharacters(string, container, options) {
   container.style.lineHeight = options.size;
 
   var result = '';
-  var characters = getMappedLetters(string, options.display === 'pronunciation');
+  var characters = Zirka.getMappedLetters(string, options.display === 'pronunciation');
   for (var i = 0; i < characters.length; i++) {
     var character = characters[i]
     if (!character) {
@@ -248,7 +250,7 @@ function insertCharacters(string, container, options) {
     }
     var filename = 'characters/' + options.type + '/' + character.character + '.svg';
     console.log(character);
-    var img = '<img src="' + getZirkaURL() + filename + '" alt="Zirka character: ' + character.pronunciation + '" style="display:inline-block;vertical-align:baseline;width:auto;height:' + options.size + ';">';
+    var img = '<img src="' + Zirka.getURL() + filename + '" alt="Zirka character: ' + character.pronunciation + '" style="display:inline-block;vertical-align:baseline;width:auto;height:' + options.size + ';">';
     switch (options.display) {
       default:
       case 'both': {
@@ -270,20 +272,20 @@ function insertCharacters(string, container, options) {
   container.innerHTML = result;
 }
 
-function getZirkaURL() {
-  if (typeof window.zirkaURL === 'undefined') {
+Zirka.getURL = function () {
+  if (typeof Zirka.url === 'undefined') {
     var scripts = document.getElementsByTagName('script');
     for (var i = 0; i < scripts.length; i++) {
       if (/translate.js$/.test(scripts[i].src)) {
-        window.zirkaURL = scripts[i].src.replace(/translate.js$/, '');
+        Zirka.url = scripts[i].src.replace(/translate.js$/, '');
         break;
       }
     }
   }
-  return window.zirkaURL;
+  return Zirka.url;
 }
 
-function translate() {
+Zirka.translate = function () {
   var zirkaElements = document.getElementsByTagName('zirka');
   for (var i = 0; i < zirkaElements.length; i++) {
     var element = zirkaElements[i];
@@ -294,7 +296,7 @@ function translate() {
     if (element.getAttribute('type')) {
       options.type = element.getAttribute('type');
     }
-    insertCharacters(element.innerText, element, options);
+    Zirka.insertCharacters(element.innerText, element, options);
   }
   var zirkaOnlyElements = document.getElementsByTagName('zirka-only');
   for (var i = 0; i < zirkaOnlyElements.length; i++) {
@@ -306,7 +308,7 @@ function translate() {
     if (element.getAttribute('type')) {
       options.type = element.getAttribute('type');
     }
-    insertCharacters(element.innerText, element, options);
+    Zirka.insertCharacters(element.innerText, element, options);
   }
   var zirkaPronounceElements = document.getElementsByTagName('zirka-pronounce');
   for (var i = 0; i < zirkaPronounceElements.length; i++) {
@@ -318,8 +320,8 @@ function translate() {
     if (element.getAttribute('type')) {
       options.type = element.getAttribute('type');
     }
-    insertCharacters(element.innerText, element, options);
+    Zirka.insertCharacters(element.innerText, element, options);
   }
 }
 
-translate();
+Zirka.translate();
